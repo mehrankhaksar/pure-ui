6e6370c9210de58c3ce4d5376a7f94c9ccb0f9cc
mehrankhaksar <mehran9326@gmail.com>
2025-10-30 15:42:57 +0330

    add accordion ui

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import "./index.css";
 
 import Tooltip from "./ui/tooltip/tooltip";
+import Accordion from "./ui/accordion/accordion";
 
 const tooltip = new Tooltip(document.querySelector(".tooltip"));
 tooltip.init();
+
+const accordions = document.querySelectorAll(".accordion");
+accordions.forEach((accordion) => {
+  const instance = new Accordion(accordion);
+  instance.init();
+});
---
+++ src/ui/accordion/accordion.js
@@ -0,0 +1,17 @@
+import "./accordion.css";
+
+class Accordion {
+  constructor(container) {
+    this.container = container;
+    this.trigger = container.querySelector(".trigger");
+    this.content = container.querySelector(".content");
+  }
+  init() {
+    this.trigger.addEventListener("click", () => {
+      this.trigger.classList.toggle("active");
+      this.content.classList.toggle("active");
+    });
+  }
+}
+
+export { Accordion as default };
